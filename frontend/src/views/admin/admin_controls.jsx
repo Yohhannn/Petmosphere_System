a2e4ea8207b3796d9a6c39db57268e9647f0ef82
rexshimura <mortpauljpm03@gmail.com>
2025-05-20 12:21:05 +0800

    Admin Controls Improved

--- frontend/src/views/admin/admin_controls.jsx
+++ frontend/src/views/admin/admin_controls.jsx
@@ -223,7 +223,7 @@ const AdminControls = () => {
           </section>
         </div>
 
-        {/* Add/Edit Modal */}
+        {/* Add/Edit Modal ni diri*/}
         {showModal && (
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
             <div className="bg-white p-6 rounded shadow-lg max-w-md w-full">
